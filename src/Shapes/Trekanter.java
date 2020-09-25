b3e1e7cd3a22da306f9437c691e6e6a961eff15a
Mia <miadong179@gmail.com>
2020-09-25 11:48:20 +0200

    TrekanterVirker

---
+++ src/Shapes/Trekanter.java
@@ -0,0 +1,72 @@
+package Shapes;
+
+import java.util.Scanner;
+
+public class Trekanter {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+
+        final double cos_A;
+        final double cos_B;
+        final double cos_C;
+        final double vinkel_A;
+        final double vinkel_B;
+        final double vinkel_C;
+
+        //Cos A
+        cos_A = (Math.pow(b,2)+Math.pow(c,2)-Math.pow(a,2))/(2*b*c);
+
+        //Cos B
+        cos_B = (Math.pow(a,2)+Math.pow(c,2)-Math.pow(b,2))/(2*a*c);
+
+        //Cos C
+        cos_C = (Math.pow(a,2)+Math.pow(b,2)-Math.pow(c,2))/(2*a*b);
+
+        //vinkel A
+        vinkel_A = Math.toDegrees(Math.acos(cos_A));
+
+        //vinkel B
+        vinkel_B = Math.toDegrees(Math.acos(cos_B));
+
+        //vinkel C
+        vinkel_C = Math.toDegrees(Math.acos(cos_C));
+
+
+        //ugyldig
+        if ( a > b + c || b > a + c || c > a + b ) {
+            System.out.println("Noget er galt");
+        }
+
+        //ligesidet trekant
+        if (a == b && b == c) {
+            System.out.println("Trekanten er ligesidet");
+        }
+
+        //ligebenet
+        if (a == b && c != b || b == c && a != c || a == c && b != c) {
+            System.out.println("Trekanten er ligebenet");
+        }
+
+        //retvinklet
+        if (vinkel_A == 90 || vinkel_B == 90 || vinkel_C == 90){
+            System.out.println("Trekanten er retvinklet");
+        }
+
+        //spidsvinklet
+        if (vinkel_A < 90 && vinkel_B < 90 && vinkel_C < 90){
+            System.out.println("Trekanten er spids");
+        }
+
+        //stumpvinklet
+        if (vinkel_A > 90 || vinkel_B > 90|| vinkel_C > 90){
+            System.out.println("Trekanten er stump");
+        }
+
+    }
+
+}
